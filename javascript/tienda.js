4df6742b4ac948bf201145011509dd89985d81a4
Briancei <brianfernandezgimenez@alumnos.cei.es>
2025-07-25 16:24:24 +0200

    rpueba3

--- javascript/tienda.js
+++ javascript/tienda.js
@@ -1,5 +1,12 @@
-// tienda.js
-(() => {
+
+const PRODUCTOS = [
+  { id: 1, nombre: "Café en grano", precio: 9.5 },
+  { id: 2, nombre: "Café en capsulas", precio: 8 },
+  { id: 3, nombre: "Café molido", precio: 8.5 },
+  { id: 4, nombre: "Set de tazas café", precio: 19.9 }
+];
+
+window.addEventListener('DOMContentLoaded', () => {
   const gridProductos = document.querySelector('.Productos');
   const carritoSidebar = document.getElementById('carritoSidebar');
   const carritoItems = document.getElementById('carritoItems');
@@ -7,26 +14,28 @@
   const carritoTotal = document.getElementById('carritoTotal');
   const botonPagar = document.getElementById('botonPagar');
   const botonCerrarCarrito = document.getElementById('botonCerrarCarrito');
-  const botonMenu = document.getElementById('botonMenu');
 
   let carrito = [];
 
-  // Renderizado inicial de carrito
   refrescarCarrito();
 
-  // eventos para botones "Añadir"
+  
   gridProductos.addEventListener('click', (e) => {
     if (e.target.matches('.Producto-boton')) {
-      const id = e.target.dataset.id;
-      const producto = PRODUCTOS.find(p => p.id == id);
-      carrito.push(producto);
-      refrescarCarrito();
-      abrirCarrito();
+      const id = parseInt(e.target.dataset.id);
+      const producto = PRODUCTOS.find(p => p.id === id);
+      if (producto) {
+        carrito.push(producto);
+        refrescarCarrito();
+        abrirCarrito();
+      }
     }
   });
 
+  // Cerrar carrito
   botonCerrarCarrito.addEventListener('click', cerrarCarrito);
 
+  // Simular pago
   botonPagar.addEventListener('click', () => {
     alert('Simulación de pago completada. ¡Gracias por tu compra!');
     carrito = [];
@@ -37,13 +46,15 @@
   function abrirCarrito() {
     carritoSidebar.classList.add('Carrito--abierto');
   }
-
+  
   function cerrarCarrito() {
     carritoSidebar.classList.remove('Carrito--abierto');
   }
 
+  
   function refrescarCarrito() {
     carritoItems.innerHTML = '';
+
     if (carrito.length === 0) {
       carritoVacio.style.display = 'block';
       carritoTotal.textContent = '';
@@ -52,6 +63,7 @@
     } else {
       carritoVacio.style.display = 'none';
       let total = 0;
+
       carrito.forEach(item => {
         total += item.precio;
         const div = document.createElement('div');
@@ -62,9 +74,10 @@
         `;
         carritoItems.appendChild(div);
       });
+
       carritoTotal.textContent = 'Total: €' + total.toFixed(2);
       botonPagar.disabled = false;
       botonPagar.classList.remove('Boton--bloqueado');
     }
   }
-})();
+});
